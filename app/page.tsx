9be81c732c4ee2b086e14aa2eda7abd4605ac7e6
John Attai Yinusa <donattai1032@gmail.com>
2023-02-10 09:53:14 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-red-500">This is the home page</h2>
+    </div>
+  );
+};
+
+export default page;
